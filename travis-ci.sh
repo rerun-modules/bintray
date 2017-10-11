a98aa51b438eff4feb7f4e2f754c470c2ad28886
Lee Thompson <lthompson@alienvault.com>
2017-10-11 01:07:54 -0500

    Put all debian generated files into repo

--- travis-ci.sh
+++ travis-ci.sh
@@ -79,10 +79,10 @@ if [[ "${TRAVIS_BRANCH}" == "master" && "${TRAVIS_PULL_REQUEST}" == "false" ]]; 
   export REPO="rerun-deb"
   rerun bintray:package-upload-deb --version "${sysver}" --file "${DEB}" --deb_architecture all
   rerun bintray:package-upload --version "${sysver}" --file "${PACKAGE}_${VERSION}.orig.tar.gz"
-  #rerun bintray:package-upload --version "${sysver}" --file "${PACKAGE}_${sysver}.debian.tar.xz"
-  #rerun bintray:package-upload --version "${sysver}" --file "${PACKAGE}_${sysver}.amd64.build"
-  #rerun bintray:package-upload --version "${sysver}" --file "${PACKAGE}_${sysver}.amd64.changes"
-  #rerun bintray:package-upload --version "${sysver}" --file "${PACKAGE}_${sysver}.dsc"
+  rerun bintray:package-upload --version "${sysver}" --file "${PACKAGE}_${sysver}.debian.tar.gz"
+  rerun bintray:package-upload --version "${sysver}" --file "${PACKAGE}_${sysver}.amd64.build"
+  rerun bintray:package-upload --version "${sysver}" --file "${PACKAGE}_${sysver}.amd64.changes"
+  rerun bintray:package-upload --version "${sysver}" --file "${PACKAGE}_${sysver}.dsc"
 
   echo "Uploading rpm package ${RPM} to bintray: /${BINTRAY_ORG}/rerun-rpm ..."
   export REPO="rerun-rpm"
